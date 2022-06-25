10c94ce0e6820fb7a9cf4ed5f44325c38cd69e82
Надежда Берестова <berestanadi@gmail.com>
2022-06-25 18:58:13 +0300

    edit datagenerator

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -21,7 +21,7 @@ public class DataGenerator {
 
     public static String generateName(String locale) {
         Faker faker = new Faker(new Locale(locale));
-        return faker.name().fullName();
+        return faker.name().lastName() + " " + faker.name().firstName();
     }
 
     public static String generatePhone(String locale) {
